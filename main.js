53e159bcac5a386b8b07183076d88e688240e486
veerdhi5080s <93766445+veerdhi5080s@users.noreply.github.com>
2022-07-14 01:36:56 -1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recognition=new SpeechRecognition();
+function start(){
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+recognition.onresult=function(event){
+    console.log(event);
+    var content=event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML=content;
+    console.log(content);
+    speak();
+}
+
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data=document.getElementById("textbox").value;
+    var Utterthis= new SpeechSynthesisUtterance(speak_data);
+    synth.speak(Utterthis);
+    Webcam.attach("#camera");
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format: "png",
+    png_quality: 100
+});
